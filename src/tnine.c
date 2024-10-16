6b42d657f1a4a17bf9d6487ffff66476fd9f4e6c
Sipxi <sipxi@email.cz>
2024-10-17 00:28:26 +0200

    Stable but BUG - more then one occurance

--- src/tnine.c
+++ src/tnine.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <unistd.h> // Include this for isatty and fileno
 
+// ? Try enum?
 #define MAX_CHARACTERS_READ 100
 #define MAX_CONTACT_INFO_LENGTH 99
 #define EXCEEDED_MAX_LENGTH -2
@@ -25,53 +26,66 @@ char *string_map[] = {
 	"wxyz"	// 9
 };
 
-typedef struct {
+typedef struct
+{
 	char name[MAX_CONTACT_INFO_LENGTH];
 	char number[MAX_CONTACT_INFO_LENGTH];
-} person;
+} contact;
 
-typedef enum {
-    NUMBER,
-    TEXT
+typedef enum
+{
+	NUMBER,
+	TEXT
 } MODE;
 
-
 // TODO COMMENT
-int charToInt(char input_character){
+int charToInt(char input_character)
+{
 	return input_character - '0';
 }
 
-int getStringLength(char *string){
+int getStringLength(char *string)
+{
 	int length = 0;
-	while (string[length] != '\0'){
+	while (string[length] != '\0')
+	{
 		length++;
 	}
 	return length;
 }
 
-void toLowerCase(char *str) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] >= 'A' && str[i] <= 'Z') {
-            str[i] += ('a' - 'A'); // Convert to lowercase
-        }
-    }
+void toLowerCase(char *string)
+{
+	for (int chr_index = 0; string[chr_index] != '\0'; chr_index++)
+	{
+		if (string[chr_index] >= 'A' && string[chr_index] <= 'Z')
+		{
+			string[chr_index] += ('a' - 'A');
+		}
+	}
 }
 
-int floatingWindowSearch(char *string, int start_index, int end_index, char search_char){
-	for (int index = start_index; index < end_index; index++){
-		if (string[index] == search_char){
+int floatingWindowSearch(char *string, int start_index, int end_index, char search_char)
+{
+	for (int index = start_index; index < end_index; index++)
+	{
+		if (string[index] == search_char)
+		{
 			return index;
 		}
 	}
 	return INDEX_ERROR;
 }
 
-//Decodes string based on string_map, and stores it in decoded_string.
-void decodeString(char *string, char *decoded_string[]){
+// Decodes string based on string_map, and stores it in decoded_string.
+void decodeString(char *string, char *decoded_string[])
+{
 	int index = 0;
-	while (string[index] != '\0'){
+	while (string[index] != '\0')
+	{
 		int number = charToInt(string[index]);
-		if (number >= 0 && number <= 9){
+		if (number >= 0 && number <= 9)
+		{
 			decoded_string[index] = string_map[number];
 		}
 		index++;
@@ -79,159 +93,218 @@ void decodeString(char *string, char *decoded_string[]){
 }
 
 // Parses user input, and returns only the first argument if it exists
-char *parseUserInput(int argc, char *argv[]){
+char *parseUserInput(int argc, char *argv[])
+{
 	return (argc > 1) ? argv[1] : NULL;
 }
 
 // Based on floatingWindowSearch returns index of first occurance of character in string
-int findFirstCharOccurance(char *string, char characters_array[], int start_index){
+int findFirstCharOccurance(char *string, char characters_array[], int start_index)
+{
 	int found_index = INDEX_ERROR;
 	int characters_length = getStringLength(characters_array);
-	for (int idx = 0; idx < characters_length; idx++){
+	for (int idx = 0; idx < characters_length; idx++)
+	{
 		found_index = floatingWindowSearch(string, start_index, getStringLength(string), characters_array[idx]);
-		if (found_index != INDEX_ERROR){
+		if (found_index != INDEX_ERROR)
+		{
 			return found_index;
 		}
-	}	
-	return INDEX_ERROR;
-}
-
-// Returns next index of character in string if it exists, otherwise returns INDEX_ERROR
-int nextChar(char string[], char character, int start_index){
-	printf("[IsNextChar] Character compared string %c to %c \n", string[start_index], character);
-	printf("[IsNextChar] Returning index : %d\n", string[start_index] == character ? start_index + 1 : INDEX_ERROR);
-	return string[start_index] == character ? start_index + 1 : INDEX_ERROR; // start_index + 1 represents next index
-	
-}
-
-// Returns next index of character_array in string if it exists, otherwise returns INDEX_ERROR
-// TODO good naming of characters?
-int isNextCharArray(char string[], char characters[], int start_index){
-	printf("[IsNextCharArr] Start index is: %d\n", start_index);
-	printf("[IsNextCharArr] got array: %s\n", characters);
-	int characters_length = getStringLength(characters);
-	int next_index = INDEX_ERROR;
-	for (int idx = 0; idx < characters_length; idx++){
-		next_index = nextChar(string, characters[idx], start_index);
-		if (next_index != INDEX_ERROR){
-			return next_index;
-		}
-
 	}
 	return INDEX_ERROR;
 }
 
-/** 
+// Returns next index of character in string if it exists, otherwise returns INDEX_ERROR
+int nextChar(char string[], char character, int start_index)
+{
+	printf("[IsNextChar] Character compared string %c to %c \n", string[start_index], character);
+	printf("[IsNextChar] Returning index : %d\n", string[start_index] == character ? start_index + 1 : INDEX_ERROR);
+	return string[start_index] == character ? start_index + 1 : INDEX_ERROR; // start_index + 1 represents next index
+}
+
+// Returns next index of character_array in string if it exists, otherwise returns INDEX_ERROR
+// TODO good naming of characters?
+int isNextCharArray(char string[], char characters[], int start_index)
+{
+	printf("[IsNextCharArr] Start index is: %d\n", start_index);
+	printf("[IsNextCharArr] got array: %s\n", characters);
+	int characters_length = getStringLength(characters);
+	int next_index = INDEX_ERROR;
+	for (int idx = 0; idx < characters_length; idx++)
+	{
+		next_index = nextChar(string, characters[idx], start_index);
+		if (next_index != INDEX_ERROR)
+		{
+			return next_index;
+		}
+	}
+	return INDEX_ERROR;
+}
+
+/**
  * @brief Finds a pattern in a string or number in given string
- * 
+ *
  * So called generic function
  * based on mode, it calls the appropriate function to find the pattern
  * mode can be either TEXT or NUMBER
  * void *characters is later converted to char** or char* based on mode
 
 */
-bool findPattern(char *string, void *characters, int characters_length, MODE mode){
+bool findPattern(char *string, void *characters, int characters_length, MODE mode)
+{
 	printf("Starting findPattern\n");
 	toLowerCase(string);
 	int start_index = 0;
 	int found_index;
 
-	if (mode == NUMBER){
-		found_index = floatingWindowSearch(string, start_index, getStringLength(string),((char *)characters)[0]);
+	if (mode == NUMBER)
+	{
+		found_index = floatingWindowSearch(string, start_index, getStringLength(string), ((char *)characters)[0]);
 	}
-	else if (mode == TEXT){
+	else if (mode == TEXT)
+	{
 		found_index = findFirstCharOccurance(string, ((char **)characters)[0], start_index);
 	}
-	if (found_index == INDEX_ERROR){
+	if (found_index == INDEX_ERROR)
+	{
 		return false;
 	}
-	int next_index = found_index +1;
+	int next_index = found_index + 1;
 	printf("-----------------\n");
 	printf("In string: %s\n", string);
 	printf("Found index: %d\n", found_index);
-	if (mode == NUMBER){
+	if (mode == NUMBER)
+	{
 		printf("Digit found: %c\n", string[found_index]);
 	}
-	else if (mode == TEXT){
-		printf("Character found %c\n", string[found_index]);	
-		
+	else if (mode == TEXT)
+	{
+		printf("Character found %c\n", string[found_index]);
 	}
 	printf("First pattern found, continuing...\n");
 	printf("Next index is: %d\n", next_index);
 	printf("-----------------\n");
 
-	for (int idx = 1; idx < characters_length; idx++){
-		if (mode == NUMBER){
+	for (int idx = 1; idx < characters_length; idx++)
+	{
+		if (mode == NUMBER)
+		{
 			next_index = nextChar(string, ((char *)characters)[idx], next_index);
 		}
-		else if (mode == TEXT){
+		else if (mode == TEXT)
+		{
 			next_index = isNextCharArray(string, ((char **)characters)[idx], next_index);
 		}
-		if (next_index == INDEX_ERROR){
+		if (next_index == INDEX_ERROR)
+		{
 			return false;
 		}
 	}
 	printf("-----------------\n");
-
 
 	return found_index > INDEX_ERROR ? true : false;
 }
 
 // Function to read contacts
 // TODO MAX CONTACT check if it works
-int readContacts(person contacts[]) {
+int readContacts(contact contacts[])
+{
 	int count = 0;
-	while (count < MAX_CONTACT_INFO_LENGTH){
-		if (fgets(contacts[count].name, MAX_CHARACTERS_READ, stdin) == NULL){
+	while (count < MAX_CONTACT_INFO_LENGTH)
+	{
+		if (fgets(contacts[count].name, MAX_CHARACTERS_READ, stdin) == NULL)
+		{
 			return count; // END OF FILE, return count of contacts read
 		}
 		contacts[count].name[strcspn(contacts[count].name, "\n")] = '\0'; // Remove newline character
 
-
-		if (getStringLength(contacts[count].name) >= MAX_CONTACT_INFO_LENGTH){
+		if (getStringLength(contacts[count].name) >= MAX_CONTACT_INFO_LENGTH)
+		{
 			return EXCEEDED_MAX_LENGTH; // Name too long, return error code
 		}
 		fgets(contacts[count].number, MAX_CHARACTERS_READ, stdin);
 
 		contacts[count].number[strcspn(contacts[count].number, "\n")] = '\0'; // Remove newline character
 
-		if (getStringLength(contacts[count].number) >= MAX_CONTACT_INFO_LENGTH){
+		if (getStringLength(contacts[count].number) >= MAX_CONTACT_INFO_LENGTH)
+		{
 			return EXCEEDED_MAX_LENGTH; // Number too long, return error code
 		}
 		count++;
-
 	}
 	return count;
 }
 
 // Function to print contacts
-void printContacts(person contacts[], int count) {
+void printContacts(contact contacts[], int count)
+{
 	printf("\n--- List of Contacts ---\n");
-	for (int i = 0; i < count; i++) {
-		printf("Contact %d: Number: %s, Name: %s\n", i + 1, contacts[i].number, contacts[i].name);
+	for (int i = 0; i < count; i++)
+	{
+		printf("Contact %d: Number: %s Name: %s\n", i + 1, contacts[i].number, contacts[i].name);
+	}
+	printf("-----------------\n");
+}
+
+void findContacts(contact contacts[], int total_contacts, char *raw_input, contact found_contacts[])
+{
+	int raw_input_length = getStringLength(raw_input);
+	char *decoded_input[raw_input_length];
+	decodeString(raw_input, decoded_input);
+
+	for (int idx = 0; idx < raw_input_length; idx++)
+	{
+		printf(" {%s} \n", decoded_input[idx]);
+	}
+
+	for (int count = 0; count < total_contacts; count++)
+	{
+		if (findPattern(contacts[count].number, raw_input, raw_input_length, NUMBER))
+		{
+			printf("Found number at %s !!!!", contacts[count].name);
+			found_contacts[count] = contacts[count];
+		}
+		else
+		{
+			if (findPattern(contacts[count].name, decoded_input, raw_input_length, TEXT))
+			{
+				found_contacts[count] = contacts[count];
+			}
+		}
 	}
 }
 
+int main(int argc, char *argv[])
+{
+		/*
+	contact contacts[MAX_CONTACT_INFO_LENGTH];
+	contact found_contacts[MAX_CONTACT_INFO_LENGTH];
 
-
-int main(int argc, char *argv[]){
-	person contacts[MAX_CONTACT_INFO_LENGTH];
-
-    int totalContacts = readContacts(contacts);
-	if (totalContacts == EXCEEDED_MAX_LENGTH){
+	int total_contacts = readContacts(contacts);
+	if (total_contacts == EXCEEDED_MAX_LENGTH)
+	{
 		printf("Error: Name or number is too long\n");
 		return EXIT_ERROR;
 	}
+	printContacts(contacts, total_contacts);
 
-	printContacts(contacts, totalContacts);
+	if (argc <= 1)
+	{
+		printContacts(contacts, total_contacts);
+	}
+	else
+	{
+		char *raw_input = parseUserInput(argc, argv);
+		findContacts(contacts, total_contacts, raw_input, found_contacts);
+		printContacts(found_contacts, total_contacts);
+	}
+	*/
 
-
-
-	/*
-	person testPerson;
-	strcpy(testPerson.name, "Petr Novak");
-	strcpy(testPerson.number, "0123456789");
 	
+	contact testPerson;
+	strcpy(testPerson.name, "Petr Novak");
+	strcpy(testPerson.number, "541141120");
+
 	char *raw_input = parseUserInput(argc, argv);
 	int raw_input_length = getStringLength(raw_input);
 	int result = 0;
@@ -256,8 +329,7 @@ int main(int argc, char *argv[]){
 	}
 
 	printf("result %d \n   ", result);
-	*/
 	
+
 	return EXIT_SUCCESS;
 }
-
--- src/types.h
+++ src/types.h
@@ -10,16 +10,5 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-typedef struct contact {
-	char *name;
-	char *number;
-	
-}contact;
-bool test_find(char* string, int length, char* characters[]);
-bool foundByCharArr(char* string, int length, char* characters);
-bool findContact(char *contactInfo, char *characters[], int chararters_length);
-int floatingWindowSearch(char* string, int start_index, int end_index, char search_char);
-char* parseUserInput(int argc, char *argv[]);
-int findFirstCharOccurance(char *string, char characters[], int start_index);
 
 #endif
